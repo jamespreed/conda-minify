450ee89bd3181d87cd891475d504560c218d5b2f
james <jamesreed.physics@gmail.com>
2020-01-13 12:52:37 +0100

    add DAG

--- condamap/graph.py
+++ condamap/graph.py
@@ -1,11 +1,11 @@
-class DiGraph:
+class DirectedGraph:
     """
     A simple unweighted directed graph.  
     """
 
     def __init__(self):
         """
-        A DiGraph holds information about the directed connections
+        A DirectedGraph holds information about the directed connections
         between nodes.
         """
         self._outward = {}
@@ -57,9 +57,7 @@ class DiGraph:
         d = self._norm(dest)
         return bool(d in self._outward.get(s, set()))
 
-    def add_connections(self, 
-                        source_name, 
-                        dest_names):
+    def add_connections(self, source_name, dest_names):
         """
         Adds multiple connections from `source` to each dest in `dest_list`.
 
@@ -73,3 +71,87 @@ class DiGraph:
             self.add_edge(source_name, dest_name)
             for dest_name in dest_names
         ]
+
+    def find_path(self, start, end, path=None):
+        """
+        Finds the first available path from start to end; not always the 
+        shortest.
+        """
+        if not path:
+            path = []
+        path = path + [start]
+        if start == end:
+            return path
+        if not self._outward[start]:
+            return []
+        for node in self._outward[start]:
+            if node not in path:
+                newpath = self.find_path(node, end, path)
+                if newpath: 
+                    return newpath
+        return None
+
+class DirectedAcyclicGraph(DirectedGraph):
+    """
+    A simple unweighted directed acyclic graph.  
+    """
+    def __init__(self, on_cycle='ignore'):
+        """
+        A DAG holds information about the directed connections between nodes, 
+        but does not allow cycles.
+
+        :cycles: [str ('ignore', 'raise')]
+            How to handle the attempt to add a cycle to the graph.  
+              ignore - ignores the edge, does not add it to the graph.
+              raise  - raises an error.
+        """
+        self._on_cycle = None
+        self.on_cycle = on_cycle
+        super().__init__()
+
+    @property
+    def on_cycle(self):
+        return self._on_cycle
+
+    @on_cycle.setter
+    def on_cycle(self, value):
+        allowed = ('ignore', 'raise')
+        if value not in allowed:
+            raise ValueError(
+                'The cycles property can only have values from: '
+                '{0}'.format(allowed)
+            )
+        self._on_cycle = value
+
+    def add_edge(self, source, dest):
+        """
+        Adds an edge between two nodes to the graph.  Both `source` and `dest`
+        will be converted to lower-case strings.
+
+        Returns: source_node, dest_node
+        """
+        self.add_node(source)
+        self.add_node(dest)
+        if self._check_cycle(source, dest):
+            return super().add_edge(source, dest)
+        return None
+
+    def _detect_backedge(self, source, dest):
+        """
+        Returns True if adding an edge from `s` to `d` would create a 
+        backedge which instantiate a cycle.
+        """
+        # try to run a path from d to s, if connected, it would be a backedge 
+        return bool(self.find_path(dest, source))
+
+    def _check_cycle(self, source, dest):
+        c = self._detect_backedge(source, dest)
+        if not c:
+            return True
+        if c and (self.on_cycle == 'ignore'):
+            return False
+        raise CycleError('Add an edge from "{0}" to "{1}" would create a '
+            'cycle in the DirectedAcyclicGraph'.format(source, dest))
+
+class CycleError(ValueError):
+    pass
